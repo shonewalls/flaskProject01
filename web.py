f0fa85930a8ed50a0af603d345652511e621cadd
shonewalls <pzhou2442@gmail.com>
2023-12-07 23:59:26 +0800

    Update web.py

--- web.py
+++ web.py
@@ -1,29 +1,29 @@
+
 import csv
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/homePage')
+@app.route('/index')
 def index():
     return render_template('homePage.html')
 
 @app.route('/')
 def hello_world(): 
-    return 'nihao!'
+    return 'Hello World!'
 
 @app.route('/api/mydata', methods=['GET'])
 def api_mydata():
     data = get_data()
     return jsonify(data)
 
-
-@app.route('/us-cities/<name>')
+@app.route('/city/<name>')
 def city(name):
     with open('static/us-cities.csv', 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
             if row['city'] == name:
-                return render_template('us-cities.html', **row)
+                return render_template('homePage.html', **row)
         return "City not found", 404
 
 def get_data():
@@ -33,7 +33,6 @@ def get_data():
         for row in reader:
             data.append(row)
     return data
-
 
 
 
